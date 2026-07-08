765d1d82988939b19e743ccc7e3a5de3ebdb694e
mrsudarshanrai <rrraishati@gmail.com>
2026-07-08 15:21:46 +1000

    refactor lsp.lua to remove deprecated tsserver and streamline inlay hint setup

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,35 +1,21 @@
 return {
   "neovim/nvim-lspconfig",
-  vim.lsp.inlay_hint.enable(false),
-  dependencies = {
-    "jose-elias-alvarez/typescript.nvim",
-    init = function()
-      require("lazyvim.util").lsp.on_attach(function(_, buffer)
-          -- stylua: ignore
-          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-        vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-      end)
-    end,
-  },
-  ---@class PluginLspOpts
+  init = function()
+    -- Disables inlay hints globally on startup safely
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(args)
+        vim.lsp.inlay_hint.enable(false, { bufnr = args.buf })
+      end,
+    })
+  end,
   opts = {
-    ---@type lspconfig.options
     inlay_hints = { enabled = false },
     servers = {
-      -- tsserver will be automatically installed with mason and loaded with lspconfig
-      tsserver = {},
+      -- Use ts_ls instead of the deprecated tsserver
+      ts_ls = {},
     },
-    -- you can do any additional lsp server setup here
-    -- return true if you don't want this server to be setup with lspconfig
-    ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
     setup = {
-      -- example to setup with typescript.nvim
-      tsserver = function(_, opts)
-        require("typescript").setup({ server = opts })
-        return true
-      end,
-      -- Specify * to use this function as a fallback for any server
-      -- ["*"] = function(server, opts) end,
+      -- Let LazyVim handle standard ts_ls setup automatically
     },
   },
 }
