5a329dfa4ef101bbeeba617635d2537df212fc8c
mrsudarshanrai <rrraishati@gmail.com>
2026-07-10 13:09:53 +1000

    refactor: remove lazy.nvim setup for nvim-treesitter in init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("lazy").setup({
-  {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "master",
-    lazy = false,
-    build = "TSUpdate",
-  },
-})
